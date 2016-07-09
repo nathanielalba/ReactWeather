c1689a9fdb2c036fe56fa7a7ffd75a56202d8376
Nathaniel Alba <nathaniel.alba@gmail.com>
2016-07-09 00:09:02 -0700

    Style about page with links to stuff used in the project

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,9 +2,26 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+    <div className="text-center">
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>Here are some of the tools in this project</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a>
+        </li>
+        <li>
+          <a href="http://foundation.zurb.com/sites/">Foundation</a>
+        </li>
+      </ul>
     </div>
   );
 };
