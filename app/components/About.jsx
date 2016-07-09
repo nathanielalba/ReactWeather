45f7a668a4160329c5402207cf07a0d204339749
Nathaniel Alba <nathaniel.alba@gmail.com>
2016-07-08 21:49:27 -0700

    Fix single node loading in About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,9 +2,11 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About</h3>
-    <p>Welcome to the about page!</p>
-  )
+    <div>
+      <h3>About</h3>
+      <p>Welcome to the about page!</p>
+    </div>
+  );
 };
 
 module.exports = About;
