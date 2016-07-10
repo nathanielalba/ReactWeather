9bc67ded70368358e6f556cf3294fc0ebd86c84b
Nathaniel Alba <nathaniel.alba@gmail.com>
2016-07-09 23:01:08 -0700

    Add styling to pages, and included in app.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,9 @@ module.exports = {
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       About: 'app/components/About.jsx',
       Examples: 'app/components/Examples.jsx',
-      openWeatherMap: 'api/openWeatherMap.js',
-      ErrorModal: 'app/components/ErrorModal.jsx'
+      openWeatherMap: 'app/api/openWeatherMap.js',
+      ErrorModal: 'app/components/ErrorModal.jsx',
+      applicationStyles: 'app/styles/app.css'
     },
     extensions: ['', '.js', '.jsx']
   },
